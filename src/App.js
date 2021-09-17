8ddb2cb82abea2c2a3bb426ead57c28bf1e2bff4
Ian Coelho <ian.coel@hotmail.com>
2021-09-16 21:54:41 -0300

    header completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Api from './api/Api';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />=
+          <Route path="/login" element={<Login />} />=
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
