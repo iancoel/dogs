058278ac9257d482f81db7a32fe5b82e70f4da15
Ian Coelho <ian.coel@hotmail.com>
2021-09-22 18:30:15 -0300

    login criar

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um e-mail válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'Sua senha deverá conter ao menos 1 dígito, 1 letra maiúscula, 1 letra minúscula e no mínimo 8 caracteres',
+  },
 };
 
 const useForm = (type) => {
@@ -14,7 +19,7 @@ const useForm = (type) => {
 
   function validate(value) {
     if (type === false) return true;
-    if (value.lenght === 0) {
+    if (value.length === 0) {
       setError('Preencha um valor');
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
