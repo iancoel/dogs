c23820dea2f36bbf660f9adf5026ca59eddf8142
Ian Coelho <ian.coel@hotmail.com>
2021-09-24 09:53:51 -0300

    mobile menu

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
 import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   const activeStyle = {
     background: '#fff',
@@ -19,34 +27,56 @@ const UserHeaderNav = () => {
   };
 
   return (
-    <nav className={styles.nav}>
-      <NavLink
-        to="/conta"
-        end
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+        ></button>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
       >
-        <MinhasFotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink
-        to="/conta/estatisticas"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        <Estatisticas />
-        {mobile && 'Estatísticas'}
-      </NavLink>
-      <NavLink
-        to="/conta/postar"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        <AdicionarFoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Sair />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+        <NavLink
+          to="/conta"
+          end
+          style={({ isActive }) =>
+            isActive && !mobile ? activeStyle : undefined
+          }
+        >
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink
+          to="/conta/estatisticas"
+          style={({ isActive }) =>
+            isActive && !mobile ? activeStyle : undefined
+          }
+        >
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink
+          to="/conta/postar"
+          style={({ isActive }) =>
+            isActive && !mobile ? activeStyle : undefined
+          }
+        >
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
