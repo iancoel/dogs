70e62abf3d694e1e625286bba4fd8227017b4b22
Ian Coelho <ian.coel@hotmail.com>
2021-09-28 09:23:22 -0300

    document title

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -6,12 +6,14 @@ import UserStats from './UserStats';
 import Feed from '../Feed/Feed';
 import { UserContext } from '../../UserContext';
 import NotFound from '../NotFound';
+import Head from '../Helper/Head';
 
 const User = () => {
   const { data } = React.useContext(UserContext);
 
   return (
     <section className="container">
+      <Head title="Minha Conta" />
       <UserHeader />
       <Routes>
         <Route path="/" element={<Feed user={data.id} />} />
