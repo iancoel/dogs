7bb5ce2befe079c459ec11a3f529ab3dd55e2242
Ian Coelho <ian.coel@hotmail.com>
2021-09-28 10:33:27 -0300

    victory

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { GET_STATS } from '../../api';
+import useFetch from '../../Hooks/useFetch';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
